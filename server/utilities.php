962238a946c6412a963cbb2405dc6c0b5bd6458c
Ethan Freeburg <freeburgernator@gmail.com>
2016-12-05 12:39:16 -0600

    Change db locations for localhost

--- server/utilities.php
+++ server/utilities.php
@@ -1,17 +1,17 @@
 <?php
 	function db_Query($query){
-		$con = mysqli_connect('localhost','root','root','mathrelay3');
+		$con = mysqli_connect('localhost','root','','mathrelay4');
 		$result = mysqli_query($con, $query);
-	
+
 		if (!$result) {
 			print mysqli_error($con);
-			die("Query failed\n"); 
+			die("Query failed\n");
 		}
-		
+
 		mysqli_close($con);
 		return $result;
-	} 
-	
+	}
+
 	function checkForOption($class,$name,$default){
 		$reso = db_Query("SELECT * FROM relay_options WHERE class='$class' AND name='$name';");
 		$num = mysqli_num_rows($reso);
@@ -20,12 +20,12 @@
 		}
 		return true;
 	}
-	
+
 	function getOption($class,$name){
 		$ret = mysqli_fetch_row(db_Query("SELECT value FROM relay_options WHERE class='$class' AND name='$name';"));
 		return $ret[0];
 	}
-	
+
 	function setOption($class,$name,$value){
 		db_Query("UPDATE relay_options SET value='$value' WHERE class='$class' AND name='$name';");
 		return true;
