6fbfb78c3e3b5ab4dea1ffb6379d9bd986f1b42d
ippinator <ippinator@gmail.com>
2017-03-22 14:43:01 -0500

    changes finish page css

--- finish_page.php
+++ finish_page.php
@@ -30,50 +30,63 @@
 	</head>
 
 	<body>
-		<section>
-		<h1 id='pageTitle'> Congratulations! </h1><br>
-		<div id = "congratulations">
-			<?php
-				$team_ID = $_SESSION['teamID'];
-				$teamRank = mysqli_fetch_row(db_Query("SELECT `rank_final` FROM `team_data` WHERE `team_id`='" . $team_ID . "';"));
-				$teamRank = $teamRank[0];
-					if($teamRank<20){
-						if($teamRank>10 && $teamRank<20){
-							$finalRank = $teamRank . "th";
-							print "<span>You have finished the Math Relay! Your team ranked " . $finalRank . "!</span>";
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="blackBody">
+						<div class="leftSide">
+							<div class="pageTitle">
+								Congratulations! 
+							</div>
 
-						}else{
-							$temp = $teamRank%10;
-							switch($temp){
-								case 1:	$finalRank = $teamRank . "st"; break;
-								case 2: $finalRank = $teamRank . "nd"; break;
-								case 3: $finalRank = $teamRank . "rd"; break;
-								default: $finalRank = $teamRank . "th"; break;
-							}
-							print "<span>You have finished the Math Relay! Your team ranked " . $finalRank . "!</span>";
-						}
-					}else{
-						print "<span>You have finished the Math Relay! Thanks for participating!</span>";
-					}
-			?>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="rightSide">
+						<div id = "congratulations">
+							<?php
+								$team_ID = $_SESSION['teamID'];
+								$teamRank = mysqli_fetch_row(db_Query("SELECT `rank_final` FROM `team_data` WHERE `team_id`='" . $team_ID . "';"));
+								$teamRank = $teamRank[0];
+									if($teamRank<20){
+										if($teamRank>10 && $teamRank<20){
+											$finalRank = $teamRank . "th";
+											print "<span>Your team ranked " . $finalRank . "!</span>";
+
+										}else{
+											$temp = $teamRank%10;
+											switch($temp){
+												case 1:	$finalRank = $teamRank . "st"; break;
+												case 2: $finalRank = $teamRank . "nd"; break;
+												case 3: $finalRank = $teamRank . "rd"; break;
+												default: $finalRank = $teamRank . "th"; break;
+											}
+											print "<span>Your team ranked " . $finalRank . "!</span>";
+										}
+									}else{
+										print "<span>Thanks for participating!</span>";
+									}
+							?>
+						</div>
+						<div id = "cleanUpParagraph">
+							<?php
+								$fileName = "server/cleanupParagraph.txt";
+								$myfile = fopen($fileName,'r');
+								$text = fread($myfile, filesize($fileName));
+								fclose($myfile);
+								print "<div id='cleanup'>".$text."</div>";
+							?>
+						</div>
+
+
+
+						<button id="returnTitle">Return to Title Page</button>
+					</div>
+
+
+				</div>
+			</div>
 		</div>
-		<br>
-		<br>
-		<br>
-		<!-- We can probably run the PHP from here and not a JQUERY post, except to reject the page if too premature -->
-		<div id = "cleanUpParagraph">
-			<?php
-				$fileName = "server/cleanupParagraph.txt";
-				$myfile = fopen($fileName,'r');
-				$text = fread($myfile, filesize($fileName));
-				fclose($myfile);
-				print "<div id='cleanup'>".$text."</div>";
-			?>
-		</div>
-		<div>
-			<br>
-			<button id="returnTitle">Return to Title Page</button>
-		</div>
-	</section>
 	</body>
 </html>
